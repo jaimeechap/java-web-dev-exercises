13b6feb34cc45d8851dc08f8497eb6379db5b47f
jaimeechap <jaimee.chapman217@gmail.com>
2022-01-03 20:18:13 -0600

    Dylan's explanation

--- src/org/launchcode/java/studios/areaofacircle/AreaTwo.java
+++ src/org/launchcode/java/studios/areaofacircle/AreaTwo.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 
 public class AreaTwo {
     public static void main(String[] args) {
-    Double radius;
 
     Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the radius of the circle in cm: ");
+        Double radius;
+        do {
+            System.out.println("Please enter the radius of the circle in cm: ");
 
-        radius= input.nextDouble();
-        while (radius < 0 || radius.isNaN()) {
-            System.out.println("That is invalid input. Please enter a number greater than zero.");
-        }
-//        if (radius < 0 || radius.isNaN()) {
+//        radius= input.nextDouble();
+//        while (radius < 0 || radius.isNaN()) {
 //            System.out.println("That is invalid input. Please enter a number greater than zero.");
-//            System.out.println("Please enter the radius of the circle in cm: ");
-//        } else {
-//            radius= input.nextDouble();
 //        }
-
+            while (!input.hasNextDouble()) {
+                System.out.println("Please enter a valid radius.");
+                input.next();
+            }
+            radius = input.nextDouble();
+        } while (radius < 0);
+        input.close();
         Double area = Circle.getArea(radius);
 
-        input.close();
 
         System.out.println("The area of a circle with a radius of " + radius +"cm is " + area + " centimeters squared.");
 
